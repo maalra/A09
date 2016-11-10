f09bfa7da8bff833e927b0791cbfb136f3fbc47b
Marc Alapont Raga <maalra01@floridauniversitaria.es>
2016-11-10 12:15:16 +0100

    first commit

---
+++ 09.php
@@ -0,0 +1,15 @@
+<?php
+$tablas=[
+  "primera"=>5,
+  "segunda"=>13,
+  "tercera"=11
+];
+
+foreach($tablas as $valor){
+  echo "<hr>";
+  for ($numero=1; $numero <11; $numero++){
+      $resultado=$valor*$numero;
+    echo $valor ." x ".$numero ."=".$resultado ."<br>";
+  }
+}
+?>
